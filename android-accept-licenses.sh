49bc0c4557eccb80ff84aeadc6d98cebd8f5c624
Oleksandr Shepeliev <a.shepeliev@gmail.com>
2018-09-21 16:57:21 +0300

    Refactored android-accept-licensies.sh script

--- android-accept-licenses.sh
+++ android-accept-licenses.sh
@@ -2,13 +2,11 @@
 
 set timeout 1800
 set cmd [lindex $argv 0]
-set licenses [lindex $argv 1]
 
 spawn {*}$cmd
-expect {
-  "Do you accept the license '*'*" {
-        exp_send "y\r"
-        exp_continue
+expect { "Do you accept the license '*'*" {
+    exp_send "y\r"
+    exp_continue
   }
   eof
 }
