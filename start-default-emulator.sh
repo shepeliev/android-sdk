a4c12728e2612c5150cea4a8a806b689c93cba57
sasha <sasha@ubuntu-18.04-desktop>
2018-09-24 19:41:23 +0300

    Fix typo, make log more verbose

--- start-default-emulator.sh
+++ start-default-emulator.sh
@@ -5,7 +5,9 @@ $ANDROID_HOME/emulator/emulator @default -no-window -no-audio 2>&1 &
 
 EMU_BOOTED="no"
 while [[ ${EMU_BOOTED} != *"stopped"* ]]; do
-    echo "Wating for emulator is booted..."
+    echo "Wating for emulator booted..."
     sleep 5
-    EMU_BOOTED=$(abd shell getprop init.svc.bootanim || echo "no")
+    EMU_BOOTED=$(adb shell getprop init.svc.bootanim || echo "no")
 done
+
+echo "Emulator booted"
